80c3cf66650918e7d03e507bc8dc8a2486e703d9
jardix <jaridox@hotmail.com>
2021-06-28 11:30:37 -0700

    reset colors back to random

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
-    const g = 0;
+    const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
@@ -13,4 +13,9 @@ const intvervalId = setInterval(function () {
     for (let letter of letters) {
         letter.style.color = randomRGB();
     }
+}, 1000);
+
+const body = document.querySelector('body');
+const intervalId2 = setInterval(function () {
+    body.style.background = randomRGB();
 }, 1000);
